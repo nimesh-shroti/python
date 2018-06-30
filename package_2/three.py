9ddf4a96cc6181d10c90a7c1934fdff3eab83fe7
nimesh-shroti <nimesh.shroti25@gmail.com>
2018-06-30 13:12:53 +0530

    Adding Third File

---
+++ package_2/three.py
@@ -0,0 +1,7 @@
+#import sys
+#sys.path.append('C:\\Users\\User\\Desktop\\Python\\python-sessions\\students_exercises\\package_1')
+
+#print(sys.path)
+
+import one
+print("inside three......." ,__name__)
